2001d256d29246fb46df2830e8cac587dcc69f39
mmacheerpuppy <callum.leach@hotmail.co.uk>
2019-04-30 13:35:31 +0100

    Refactor

--- lib/mmacheerpuppy_io_api/ip_info/hosts.ex
+++ lib/mmacheerpuppy_io_api/ip_info/hosts.ex
@@ -1,6 +1,5 @@
 defmodule MmacheerpuppyIoApi.IPInfo do
   alias MmacheerpuppyIoApi.IPInfo
-  @derive {Jason.Encoder, only: [:ip, :city, :region]}
   defstruct ip: "127.0.0.1", city: "Toronto", region: "Canada"
 
   @spec resolve_location(
@@ -17,18 +16,16 @@ defmodule MmacheerpuppyIoApi.IPInfo do
               optional(:request_url) => any(),
               optional(:status_code) => integer()
             }
+
   def resolve_location(ipv4) when is_tuple(ipv4) do
     headers = [{"Accept", "application/json"}]
     params = [token: Application.get_env(:app_name, AppName.Endpoint)[:api_prefix]]
+    request = HTTPoison.get("ipinfo.io/" <> to_string(:inet.ntoa(ipv4)), headers, params: params)
 
-    case with {:ok, %IPInfo{ip: ip, city: city, region: region}} <-
-                HTTPoison.get("ipinfo.io/" <> to_string(:inet.ntoa(ipv4)), headers, params: params),
-              do: {:ok, %{ip: ip, city: city, region: region}} do
-      {:ok, request} ->
-        request
-
-      {:error, _} ->
-        nil
+    with {:ok, response = %IPInfo{}} <- HTTPoison.get(Jason.decode(request.body)) do
+      response
     end
+
+    # :error
   end
 end
